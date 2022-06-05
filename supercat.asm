58fbf3c3bd2fa2c9bab4970b00547b698a18d1f0
Tim Ryan <id@timryan.org>
2022-06-05 12:23:33 -0400

    More comments

--- supercat.asm
+++ supercat.asm
@@ -1,31 +1,42 @@
     include "bitfuncs.inc"
 
+; Unused/unknown
 zp_00 equ $00
 zp_01 equ $01
 zp_02 equ $02
 zp_03 equ $03
 zp_04 equ $04
+
 zp_irq_jmp equ $05
 zp_irq_lo equ $06
 zp_irq_hi equ $07
-zp_08 equ $08
+zp_temp_a equ $08
 zp_09 equ $09
-zp_0a equ $0a
+zp_temp_x equ $0a
 zp_0b equ $0b
 zp_0c equ $0c
-zp_0d equ $0d
+zp_frame_index equ $0d
 zp_0e equ $0e
 zp_0f equ $0f
-zp_10 equ $10
-zp_11 equ $11
+
+zp_joypad_a equ $10
+zp_joypad_b equ $11
 
 PpuControl_2000 equ $2000
+
 PPUMASK equ $2001
+PPUMASK_GREYSCALE equ %1
+PPUMASK_SHOWBG equ %1000
+PPUMASK_SHOWSP equ %10000
+PPUMASK_EMPHRED equ %100000
+PPUMASK_EMPHGREEN equ %1000000
+PPUMASK_EMPHBLUE equ %10000000
+
 PpuStatus_2002 equ $2002
 PpuScroll_2005 equ $2005
 PpuAddr_2006 equ $2006
 PpuData_2007 equ $2007
-SpriteDma_4014 equ $4014
+OAMDMA equ $4014
 
 APUSTATUS equ $4015
 APUSTATUS_ENABLE_DMC = %10000
@@ -114,8 +125,10 @@ reset:
         inx
         bne -
 
+        ; Write out all zeroes to OAM.
         lda #$07
-        sta SpriteDma_4014
+        sta OAMDMA
+
         lda #$2A
         sta PpuControl_2000
         lda #$20
@@ -146,28 +159,30 @@ reset:
         sta PpuData_2007
         sta PpuData_2007
 
-        jmp routine_8233
+        jmp frame_loop
 
         
 ; ----------------
 
         JUMP_SLIDE 40
 irq_row_dark:
-        sta zp_08
+        sta zp_temp_a
+        ; Update DMC with P1 rate.
         lda #DMCFREQ_IRQ | DMCFREQ_RATE380
         sta DMCFREQ
-        stx zp_0A
+        ; Now preserve X register as well.
+        stx zp_temp_x
 
         ; Change PPUMASK twice in quick succession to see a visible artifact.
-        lda #%00010001
+        lda #PPUMASK_GREYSCALE | PPUMASK_SHOWSP
         sta PPUMASK
-        lda #%01110000
+        lda #PPUMASK_EMPHRED | PPUMASK_EMPHGREEN | PPUMASK_SHOWSP
         sta PPUMASK
 
-        lda #$10
+        lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
 
-        ; Advance IRQ one jump cycle
+        ; Advance IRQ one jump cycle.
         lda zp_irq_lo
         clc
         adc #3
@@ -175,12 +190,13 @@ irq_row_dark:
 
         jsr sleep_36_cycles
 
-        ldx zp_0A
-
+        ; Restore X register.
+        ldx zp_temp_x
+        ; Update DMC with P2 rate.
         lda #DMCFREQ_IRQ | DMCFREQ_RATE72
         sta DMCFREQ
 
-        lda zp_08
+        lda zp_temp_a
         rti
 
 
@@ -188,17 +204,17 @@ irq_row_dark:
 
         JUMP_SLIDE 40
 irq_row_light:
-        sta zp_08
-
+        sta zp_temp_a
+        ; Update DMC with P1 rate.
         lda #DMCFREQ_IRQ | DMCFREQ_RATE428
         sta DMCFREQ
+        ; Now preserve X register as well.
+        stx zp_temp_x
 
-        stx zp_0A
-
-        lda #$10
+        lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
 
-        ; Advance IRQ one jump cycle
+        ; Advance IRQ one jump cycle.
         lda zp_irq_lo
         clc
         adc #3
@@ -209,21 +225,24 @@ irq_row_light:
         nop
         nop
 
-        lda #%00010001
+        lda #PPUMASK_GREYSCALE | PPUMASK_SHOWSP
         sta PPUMASK
-        lda #%01010000
+        lda #PPUMASK_EMPHGREEN | PPUMASK_SHOWSP
         sta PPUMASK
 
         jsr sleep_36_cycles
 
-        ldx zp_0A
+        ; Restore X register.
+        ldx zp_temp_x
+        ; Update DMC with P2 rate.
         lda #DMCFREQ_IRQ | DMCFREQ_RATE72
         sta DMCFREQ
-        lda zp_08
+
+        lda zp_temp_a
         rti
 
 
-; --------data block--------
+; --------DMC frequencies--------
 
 table_frequencies_0:
         byt $80, $80, $80, $80, $80, $80
@@ -235,54 +254,54 @@ table_frequencies_3:
         byt $86, $8D, $86, $86, $87, $89
 table_frequencies_4:
         byt $8F, $8F, $8F, $8F, $8E, $8F
-table_frequencies_5:
+
+table_frame_offset:
         byt $FF, $00, $01, $02, $FF, $01
 
 
 ; ----------------
 
 routine_8156:
-        sta zp_08
-        stx zp_0A
-        ldx zp_0D
+        sta zp_temp_a
+        stx zp_temp_x
 
+        ldx zp_frame_index
         lda table_frequencies_0,x
         sta DMCFREQ
 
-        lda #%00010001
+        lda #PPUMASK_GREYSCALE | PPUMASK_SHOWSP
         sta PPUMASK
         
         lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
         
         ; Advance IRQ trampoline
-        lda #lo(routine_8300_8175)
+        lda #lo(routine_irq_8175)
         sta zp_irq_lo
         
-        ldx zp_0A
-        lda zp_08
-
+        ldx zp_temp_x
+        lda zp_temp_a
         rti
 
 
 ; ----------------
 
 routine_8175:
-        sta zp_08
-        stx zp_0A
+        sta zp_temp_a
+        stx zp_temp_x
 
-        ldx zp_0D
+        ldx zp_frame_index
         lda table_frequencies_1,X
         sta DMCFREQ
         lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
 
         ; Advance IRQ trampoline
-        lda #lo(routine_8300_818f)
+        lda #lo(routine_irq_818f)
         sta zp_irq_lo
 
-        ldx zp_0A
-        lda zp_08
+        ldx zp_temp_x
+        lda zp_temp_a
 
         rti
 
@@ -290,23 +309,24 @@ routine_8175:
 ; ----------------
 
 routine_818F:
-        sta zp_08
-        stx zp_0A
+        sta zp_temp_a
+        stx zp_temp_x
 
-        ldx zp_0D
+        ldx zp_frame_index
         lda table_frequencies_2,X
         sta DMCFREQ
         lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
 
-        jsr routine_8214
+        ; Read joypads
+        jsr routine_read_joypad
 
         ; Advance IRQ trampoline
-        lda #lo(routine_8300_81ac)
+        lda #lo(routine_irq_frame_end)
         sta zp_irq_lo
 
-        ldx zp_0A
-        lda zp_08
+        ldx zp_temp_x
+        lda zp_temp_a
         rti
 
 
@@ -315,21 +335,22 @@ routine_818F:
 ; ----------------
 
 
-routine_81AC:
-        sta zp_08
-        stx zp_0A
+routine_frame_end:
+        sta zp_temp_a
+        stx zp_temp_x
 
-        ldx zp_0D
+        ldx zp_frame_index
         lda table_frequencies_3,x
         sta DMCFREQ
 
-        lda #$D0
+        lda #PPUMASK_EMPHBLUE | PPUMASK_EMPHGREEN | PPUMASK_SHOWSP
         sta PPUMASK
         
         ; Useless load (???)
         lda #$86
 
-        lda #lo(routine_8300)
+        ; Reset IRQ trampoline
+        lda #lo(routine_irq)
         sta zp_irq_lo
 
         jsr sleep_36_cycles
@@ -339,10 +360,11 @@ routine_81AC:
         lda #APUSTATUS_ENABLE_DMC
         sta APUSTATUS
 
-        lda table_frequencies_5,x
+        ; Calculate frame adjustment based off joypad values.
+        lda table_frame_offset,x
         tax
         bpl +
-        lda zp_10
+        lda zp_joypad_a
         ldx #$01
         asl a
         bcs +
@@ -357,10 +379,10 @@ routine_81AC:
         bcs +
         ldx #$03
     +:
-        stx zp_0D
+        stx zp_frame_index
 
-        ldx zp_0A
-        lda zp_08
+        ldx zp_temp_x
+        lda zp_temp_a
         rti
 
 
@@ -372,61 +394,59 @@ nmi:
 
 
 
-; ----------------
+; --------unknown routine--------
 
 routine_81F7:
+        ; Unknown routine
         cli
         sta zp_01
-
         lda #$00
         sta zp_0F
         inc zp_04
-
         lda #$3F
         sta PpuAddr_2006
         lda #$01
         sta PpuAddr_2006
-        
-        ; Useless write (???)
         lda zp_04
-
         lda #$01
         sta PpuData_2007
-
         lda zp_01
         rti
 
 
 ; --------sub start--------
 
-routine_8214:
+routine_read_joypad:
         lda #$01
         sta JOYPADLATCH
+
         lda #$80
-        sta zp_10
-        sta zp_11
+        sta zp_joypad_a
+        sta zp_joypad_b
+
         lda #$00
         sta JOYPADLATCH
     -:
         lda JOYPADLATCH
         lsr a
-        ror zp_10
+        ror zp_joypad_a
         lda JOYPADREAD
         lsr a
-        ror zp_11
+        ror zp_joypad_b
         bcc -
+
         rts
 
 
 ; ----------------
 
-routine_8233:
-        ; store "jmp $8300" into ZP
+frame_loop:
+        ; Store "jmp $8300" into ZP.
         lda #$4C
         sta zp_irq_jmp
-        lda #lo(routine_8300)
+        lda #lo(routine_irq)
         sta zp_irq_lo
-        lda #hi(routine_8300)
+        lda #hi(routine_irq)
         sta zp_irq_hi
 
         ; Impossible write to APU sample block (???)
@@ -434,7 +454,7 @@ routine_8233:
         lda dmc_sample,X
         sta dmc_sample,X
 
-        ; Load palette colors
+        ; Load palette table into palette VRAM.
         lda #$3F
         sta PpuAddr_2006
         ldx #$00
@@ -460,9 +480,10 @@ routine_8233:
         lda #$00
         sta PpuScroll_2005
 
-        lda #$18
+        lda #PPUMASK_SHOWBG | PPUMASK_SHOWSP
         sta PPUMASK
 
+        ; Unused write (???)
         lda #$40
         sta zp_00
 
@@ -478,12 +499,13 @@ routine_8233:
         ; Useless write (???)
         ldx #$00
         stx zp_09
+
+        ; Setup DMC.
         SETMEM_DMCADDRESS DMC_SAMPLE_ADDR
         lda #0
         sta DMCLEN
         lda #DMCFREQ_IRQ | DMCFREQ_RATE428
         sta DMCFREQ
-
         ; Due to a hardware quirk, we need to write the sample length three times in a row
         ; so as not to trigger an immediate IRQ. See https://www.nesdev.org/wiki/APU_DMC
         lda #APUSTATUS_ENABLE_DMC
@@ -503,16 +525,17 @@ routine_8233:
 
         ; Repeating rough cycle counter on main thread.
         ldx #$00
-    -:
+    .loop_end:
         inc $0100,X
-        bne -
+        bne .loop_end
         inc $0101,X
-        bne -
-        jmp -
+        bne .loop_end
+        jmp .loop_end
 
 
 ; --------data block--------
 
+; PPU Palette table
 table_palette:
         byt $22, $21, $11, $31, $22, $21, $11, $31
         byt $22, $21, $11, $31, $22, $21, $11, $31
@@ -523,11 +546,11 @@ table_palette:
 
 ; ----------------
 
-    ; must be aligned to a page boundary
-    ; IRQ trampoline only rewrites the lower byte here
-    org $8300
+    ; IRQ trampoline routine must be aligned to a page boundary,
+    ; because the zero page trampoline only ever rewrites the lower byte.
+    align 256
 
-routine_8300:
+routine_irq:
         jmp irq_row_light - 2
         jmp irq_row_dark - 3
         jmp irq_row_light - 4
@@ -555,12 +578,12 @@ routine_8300:
         jmp irq_row_light - 34
         jmp irq_row_dark - 35
         jmp routine_8156
-routine_8300_8175:
+routine_irq_8175:
         jmp routine_8175
-routine_8300_818f:
+routine_irq_818f:
         jmp routine_818F
-routine_8300_81ac:
-        jmp routine_81AC
+routine_irq_frame_end:
+        jmp routine_frame_end
         rts
 
 
