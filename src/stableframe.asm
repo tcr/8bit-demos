19e364e0e4772817f33d5f526f4e4190281a8b5e
Tim Ryan <id@timryan.org>
2022-06-18 16:28:53 -0400

    Indirect jump

--- src/stableframe.asm
+++ src/stableframe.asm
@@ -736,76 +736,11 @@ IRQ_STEP macro ADDRESS
         d16 ADDRESS
     endm
 
-MACRO_IRQ_TABLE macro
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        ; skip medium
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-        IRQ_STEP irq_row_medium  - 0
-        IRQ_STEP irq_row_light   - 4
-        IRQ_STEP irq_row_dark    - 0
-
-        IRQ_STEP routine_frame_blank_start
-        IRQ_STEP routine_frame_blank_split_1
-        IRQ_STEP routine_frame_blank_split_2
-        IRQ_STEP routine_frame_end
-    endm
 
         align 256
 table_irq_0:
-        MACRO_IRQ_TABLE
+        include "irq_table.asm"
+
 
 ; table_irq_end:
 ;         align 256
