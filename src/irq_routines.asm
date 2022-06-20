dd7461e04d1e228a7100ce8bed8d768180853d64
Tim Ryan <id@timryan.org>
2022-06-20 03:42:49 -0400

    Wow stable frame

--- src/irq_routines.asm
+++ src/irq_routines.asm
@@ -46,6 +46,174 @@ irq_set_rate:
         rti
 
 
+irq_keep_rate:
+        ; Preserve registers.
+        IRQ_SAVE_REGISTERS
+
+        ; Acknowledge IRQ.
+        lda #APUSTATUS_ENABLE_DMC
+        sta APUSTATUS
+
+        ; Advance IRQ trampoline
+        IRQ_ADVANCE_LOOKUP 2
+
+        ; Restore registers and return.
+        IRQ_RESTORE_REGISTERS
+        rti
+
+
+irq_set_two_rates:
+        ; [+ 3] Preserve registers.
+        IRQ_SAVE_REGISTERS
+        ; [= 3]
+
+        ; [+10] Update DMC with P1 rate.
+        ldy #2
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [= 9]
+
+        ; [+ 5] Acknowledge IRQ.
+        lda #APUSTATUS_ENABLE_DMC
+        sta APUSTATUS
+        ; [=24]
+
+        ; [+24] Sleep.
+        SLEEP 30
+        ; [+ 5] After 54 (P0) cycles, update DMC with P2 rate.
+        iny
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [=63]
+
+        ; Advance IRQ trampoline
+        IRQ_ADVANCE_LOOKUP 4
+
+        ; Restore registers and return.
+        IRQ_RESTORE_REGISTERS
+        rti
+
+
+irq_reset_to_frame:
+        ; [+ 3] Preserve registers.
+        IRQ_SAVE_REGISTERS
+        ; [= 3]
+
+        ; [+10] Update DMC with P1 rate.
+        ldy #2
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [= 9]
+
+        ; [+10] Change PPUMASK twice in quick succession to see a visible artifact.
+        lda #PPUMASK_COMMON | PPUMASK_GREYSCALE
+        sta PPUMASK
+        lda #PPUMASK_COMMON | PPUMASK_EMPHRED
+        sta PPUMASK
+        ; [=19]
+
+        ; [+ 5] Acknowledge IRQ.
+        lda #APUSTATUS_ENABLE_DMC
+        sta APUSTATUS
+        ; [=24]
+
+        ; Reset IRQ trampoline to point to current frame section.
+        lda zp_frame_index
+        asl
+        asl
+        asl
+        asl
+        sta zp_irq_lo
+
+        ; Restore registers and return.
+        IRQ_RESTORE_REGISTERS
+        rti
+
+
+irq_set_rate_and_advance:
+        ; DMC P0=lookup2
+
+        ; [+ 6] Preserve registers.
+        IRQ_SAVE_REGISTERS
+        ; [= 6]
+
+        ; [+11] Update DMC P1 with lookup3.
+        ldy #2
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [=17]
+
+        ; [+ 5] Restore PPUMASK to start showing colors after the blanking period.
+        lda #PPUMASK_COMMON | PPUMASK_EMPHBLUE | PPUMASK_EMPHGREEN
+        sta PPUMASK
+        ; [=22]
+
+        ; Read the joypad, then update frame index based on it.
+        jsr routine_read_joypad
+        ldy zp_frame_index
+        jsr routine_update_frame_from_joypad
+
+        ; [+ 5] Acknowledge IRQ.
+        lda #APUSTATUS_ENABLE_DMC
+        sta APUSTATUS
+        ; [=34]
+
+        ; Move IRQ trampoline to point to "rows" section.
+        lda #lo(table_irq_rows)
+        sta zp_irq_lo
+
+        ; Restore registers and return.
+        IRQ_RESTORE_REGISTERS
+        rti
+
+
+irq_set_two_rates_and_advance:
+        ; DMC P0=lookup2
+
+        ; [+ 6] Preserve registers.
+        IRQ_SAVE_REGISTERS
+        ; [= 6]
+
+        ; [+11] Update DMC P1 with lookup3.
+        ldy #2
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [=17]
+
+        ; [+ 5] Restore PPUMASK to start showing colors after the blanking period.
+        lda #PPUMASK_COMMON | PPUMASK_EMPHBLUE | PPUMASK_EMPHGREEN
+        sta PPUMASK
+        ; [=22]
+
+        ; [+42]
+        SLEEP 82
+        ; [=76]
+
+        ; [+ 8] Update DMC P2 with lookup4.
+        iny
+        lda (zp_irq_lo),y
+        sta DMCFREQ
+        ; [=84]
+
+        ; Read the joypad, then update frame index based on it.
+        jsr routine_read_joypad
+        ldy zp_frame_index
+        jsr routine_update_frame_from_joypad
+
+        ; [+ 5] Acknowledge IRQ.
+        lda #APUSTATUS_ENABLE_DMC
+        sta APUSTATUS
+        ; [=34]
+
+        ; Move IRQ trampoline to point to "rows" section.
+        lda #lo(table_irq_rows)
+        sta zp_irq_lo
+
+        ; Restore registers and return.
+        IRQ_RESTORE_REGISTERS
+        rti
+
+
 ; -------irq blank routines---------
 
 irq_blank_enter:
--- src/irq_table.asm
+++ src/irq_table.asm
@@ -1,30 +1,30 @@
         align 16
 table_irq_frame_0:
-        IRQ_CALL irq_blank_enter,   DMCFREQ_IRQ_RATE428
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE214
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE54
-        IRQ_CALL irq_blank_exit,    DMCFREQ_IRQ_RATE190,    DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE128
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE160
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE226
+        IRQ_CALL irq_set_rate_and_advance, DMCFREQ_IRQ_RATE54
 
         align 16
 table_irq_frame_1:
-        IRQ_CALL irq_blank_enter,   DMCFREQ_IRQ_RATE380
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE226
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE106
-        IRQ_CALL irq_blank_exit,    DMCFREQ_IRQ_RATE72,     DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE128
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE286
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_two_rates_and_advance, DMCFREQ_IRQ_RATE428, DMCFREQ_IRQ_RATE54
 
         align 16
 table_irq_frame_2:
-        IRQ_CALL irq_blank_enter,   DMCFREQ_IRQ_RATE428
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE214
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE54
-        IRQ_CALL irq_blank_exit,    DMCFREQ_IRQ_RATE190,    DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE128
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE286
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_two_rates_and_advance, DMCFREQ_IRQ_RATE428, DMCFREQ_IRQ_RATE54
 
         align 16
 table_irq_frame_3:
-        IRQ_CALL irq_blank_enter,   DMCFREQ_IRQ_RATE428
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE214
-        IRQ_CALL irq_set_rate,      DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_blank_exit,    DMCFREQ_IRQ_RATE54,     DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE128
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE286
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE54
+        IRQ_CALL irq_set_two_rates_and_advance, DMCFREQ_IRQ_RATE428, DMCFREQ_IRQ_RATE54
 
         align 16
 table_irq_frame_4:
@@ -50,59 +50,30 @@ table_irq_rows:
         IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
         IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
         IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-
         IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
         IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
         IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
         IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
         IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
         IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
 
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
+        ; end of road
+        IRQ_CALL irq_set_two_rates, DMCFREQ_IRQ_RATE428, DMCFREQ_IRQ_RATE72
 
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        ; IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
+        ; top of map
+        rept 17
+            IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE54
+        endm
+        IRQ_CALL irq_set_two_rates, DMCFREQ_IRQ_RATE320, DMCFREQ_IRQ_RATE380
 
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
+        ; last 8 rows of map
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE72
 
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-
-        IRQ_CALL irq_light_row - 4, DMCFREQ_IRQ_RATE84
-        IRQ_CALL irq_dark_row  - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_light_row - 0, DMCFREQ_IRQ_RATE72
-        IRQ_CALL irq_dark_row  - 4, DMCFREQ_IRQ_RATE84
-
+        ; end of map
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE428
+        IRQ_CALL irq_set_rate, DMCFREQ_IRQ_RATE226
         ; Terminating row.
-        IRQ_CALL irq_last_row  - 0, DMCFREQ_IRQ_RATE72
+        IRQ_CALL irq_reset_to_frame, DMCFREQ_IRQ_RATE226
 
 
         align 16
